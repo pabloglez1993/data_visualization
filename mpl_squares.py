43dca905deffb06c63cd2b44625a1b7d36a7d143
Pablo <pablo@Pablos-MacBook-Pro.local>
2022-12-08 15:08:16 -0600

    Add colors

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,9 +1,13 @@
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-squares = [1, 4, 9, 16, 25]
+x_values = range(1, 100)
+y_values = [x**2 for x in x_values]
 
+plt.style.use('seaborn-darkgrid')
 fig, ax = plt.subplots()
-ax.plot(squares, linewidth=3)
+ax.scatter(x_values, y_values, c=y_values, cmap=mpl.colormaps['Blues'], s=10)
+# ax.plot(x_values, y_values, linewidth=3)
 
 # Set chart title and label axes.
 ax.set_title('Square Numbers', fontsize=24)
@@ -12,5 +16,8 @@ ax.set_ylabel('Square of Value', fontsize=14)
 
 # Set size of tick labels.
 ax.tick_params(axis='both', labelsize=14)
+
+# Set the range of each axis.
+ax.axis([0, 110, 0, 11000])
 
 plt.show()
