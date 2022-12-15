b7e52e4f7e385a33744cc022b77840a335091096
Pablo <pablo7.24.93@gmail.com>
2022-12-14 18:06:51 -0600

    Three dice

--- dice_visual.py
+++ dice_visual.py
@@ -5,24 +5,25 @@ from die import Die
 
 die_1 = Die()
 die_2 = Die()
+die_3 = Die()
 
 results = []
-for roll_num in range(1000):
-    result = die_1.roll() + die_2.roll()
+for roll_num in range(50000):
+    result = die_1.roll() + die_2.roll() + die_3.roll()
     results.append(result)
 
 frequencies = []
-max_results = die_1.num_sides + die_2.num_sides
-for value in range(2, max_results+1):
+max_results = die_1.num_sides + die_2.num_sides + die_3.num_sides
+for value in range(3, max_results+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # Visualize the results.
 
-x_values = list(range(2, max_results+1))
+x_values = list(range(3, max_results+1))
 data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title':'Result'}
 y_axis_config = {'title':'Frequency of Result'}
-my_layout = Layout(title='Results of rolling two D6 dice 1000 times', xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data':data,'layout':my_layout}, filename='d6_d6.html')
+my_layout = Layout(title='Results of rolling three D6 50000 times', xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data':data,'layout':my_layout}, filename='d6_d10.html')
